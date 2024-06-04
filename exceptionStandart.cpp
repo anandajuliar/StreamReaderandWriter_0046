2d3a541581778ed0e70382ae0d1d59bd45193fc0
anandajuliar <anandajulia001@gmail.com>
2024-06-04 18:26:55 +0700

    ganti data

--- exceptionStandart.cpp
+++ exceptionStandart.cpp
@@ -9,7 +9,7 @@ int main()
 {
     cout << "Awal Program" << endl; //penanda 1:...
     try {
-        array<int, 3> data = { 1, 2, 3 };
+        array<int, 3> data = { 6, 7, 8 };
         //pesan array integer 3 elemen
         cout << data.at(5) << endl;
         //memanggil array element ke 5
