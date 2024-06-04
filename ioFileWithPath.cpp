aa3e8b1e5ebc3d3564b80684b5b22aa9c4b39e04
anandajuliar <anandajulia001@gmail.com>
2024-06-04 17:44:59 +0700

    membuka dan menunjuk

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -8,3 +8,11 @@ int main ()
     string baris;
     string NamaFile;
 
+    cout << "Masukkan nama file : ";
+    cin >> NamaFile;
+
+    ofstream outfile;// membuka file dalam mode menulis.
+    outfile.open(NamaFile + ".txt", ios::out); // menunjuk ke sebuah nama file
+
+    cout << ">= Menulis File, \'q\' untuk keluar" << endl;
+
