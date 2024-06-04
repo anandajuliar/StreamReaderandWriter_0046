5c4f68db3965b1f0c77ca4cc2994fb351a7699d7
anandajuliar <anandajulia001@gmail.com>
2024-06-04 17:27:11 +0700

    unli loop untk menulis

--- ioFile.cpp
+++ ioFile.cpp
@@ -8,4 +8,18 @@ int main() {
 
     ofstream outfile; //membuka file dlm mode menulis
     outfile.open("contohfile.txt");//menunjuk kesebuah nama file
+
+    cout<< ">=menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukkan karakter q
+		if (baris == "q") break;
+		//menulis dan memasukkan niai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
+  
 }
